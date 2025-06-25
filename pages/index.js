6719fb8817c74b150c929c8341f442a1cef27395
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-25 15:30:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
+import Head from 'next/head';
+import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import WhyWeb3 from '@/components/WhyWeb3';
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <WhyWeb3 />
-    </main>
+    <>
+      <Head>
+        <title>Upskill Africa | Learn Blockchain & Web3</title>
+        <meta name="description" content="Equip yourself with Blockchain and Web3 skills through Upskill Africa. Innovate, build, and lead in the digital economy." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="Web3, Blockchain, Africa, Learn, Upskill" />
+        <meta name="author" content="Upskill Africa" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main>
+        <Hero />
+        <WhyWeb3 />
+      </main>
+    </>
   );
-}
+    }
