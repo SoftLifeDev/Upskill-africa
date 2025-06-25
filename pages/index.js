a3f3ee07cbe50cfbdf90c4fc73cbda8b2b998d64
SoftLifeDev <softlife.dev001@gmail.com>
2025-06-25 14:27:13 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Hero from '@/components/Hero';
+import WhyWeb3 from '@/components/WhyWeb3';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <WhyWeb3 />
+    </main>
+  );
+}
